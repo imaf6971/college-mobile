eea8184bf07a9cdaf0ac5f87d2f7aa26be84d5e1
Vladislav Mironov <imaf6971@gmail.com>
2023-04-08 15:36:01 +0300

    added calendar header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,29 @@
 import { View, Text } from 'react-native'
+import { Feather } from '@expo/vector-icons';
 // import { useAuth } from '../../context/auth'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function capitalize(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 export default function Index() {
   // const { signOut } = useAuth();
   const insets = useSafeAreaInsets();
 
+  const today = new Date();
+  const currentMonth = Intl.DateTimeFormat(undefined, {
+    month: 'long'
+  }).format(today);
+
   return (
-    <View style={{
-      paddingTop: insets.top,
-      paddingBottom: insets.bottom,
-      paddingLeft: insets.left,
-      paddingRight: insets.right
-    }}>
+    <View
+      style={{
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right
+      }} >
       <Text className="ml-4 text-neutral-700 mt-4 font-semibold text-2xl">
         Добро пожаловать!
       </Text>
@@ -22,7 +33,12 @@ export default function Index() {
             Ближайшие мероприятия
           </Text>
         </View>
+        <View className='w-full py-4 flex-row justify-around items-center'>
+          <Feather name='chevron-left' size={24} color="#404040" />
+          <Text className='font-semibold text-neutral-700 text-lg'>{capitalize(currentMonth)}</Text>
+          <Feather name='chevron-right' size={24} color="#404040" />
+        </View>
       </View>
-    </View>
+    </View >
   )
 }
